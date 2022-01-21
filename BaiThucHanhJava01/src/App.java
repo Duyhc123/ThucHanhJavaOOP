ec505378e5505e479cf76953ef32fc36dd738462
Duyhc123 <duy123.lqm@gmail.com>
2022-01-21 13:35:22 +0700

    first commit

---
+++ BaiThucHanhJava01/src/App.java
@@ -0,0 +1,7 @@
+package BaiThucHanhJava.BaiThucHanhJava01.src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
